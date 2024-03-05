ba5a26bd4a41ad7d3ef6071b2a1e629a198bf679
Priyanshu Bansal <103120085@nitt.edu>
2024-03-05 18:07:36 +0530

    fixed login page logo

--- src/components/LoginDashboard.jsx
+++ src/components/LoginDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginDashboard.css';
-
+import Logo from '../assets/zentrades-logo.png';
 
 function LoginDashboard() {
   const [email, setEmail] = useState('');
@@ -9,7 +9,7 @@ function LoginDashboard() {
   const [rememberMe, setRememberMe] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleOnSubmit = (e) => {
     e.preventDefault();
 
     // Email validation
@@ -41,38 +41,52 @@ function LoginDashboard() {
   };
 
   return (
-    <div className="container-wrapper">
-    <div className="container">
-      <h1>ZENTRADES</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="E-mail address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div>
-          <input
-            type="checkbox"
-            id="rememberMe"
-            checked={rememberMe}
-            onChange={(e) => setRememberMe(e.target.checked)}
-          />
-          <label htmlFor="rememberMe">Remember me</label>
+    <div className='bg-black w-screen min-h-screen'>
+    <form
+        className='h-screen flex flex-col justify-center items-center w-96 mx-auto'
+        onSubmit={handleOnSubmit}
+    >
+        <img src={Logo} width={320} alt="Zentrades Logo" />
+        <div className='w-5/6 mt-12 mb-6'>
+            <input
+                type="text"
+                placeholder="E-mail address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className='w-full rounded-t-lg p-4 active:border-primary'
+            />
+            <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className='w-full rounded-b-lg p-4'
+            />
         </div>
-        <button type="submit"> Sign In</button>
-        <div>
-          <a className='forgetPassword' href="#" onClick={handleForgotPassword}>Forgot your password?</a>
+
+        <div className='text-white flex items-center gap-1 justify-center'>
+            <input
+                type="checkbox"
+                className='block border-none rounded-sm checked:bg-secondary hover:bg-secondary active:bg-secondary checked:hover:bg-secondary checked:active:bg-secondary checked:hover:border-none checked:active:border-none'
+                checked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <p>Remember Me</p>
         </div>
-      </form>
-    </div>
-    </div>
+        <button 
+            type='submit' 
+            className='w-5/6 mt-2 mb-6 p-4 rounded bg-[#EE5569] hover:bg-red-400 text-white'
+        >Sign In</button>
+        <a
+            href='mailto:support@zentrades.pro?subject=Password%20Reset'
+            target='_blank'
+            className='text-white text-sm'
+        >Forgot your Password?</a>
+       
+    </form>
+</div>
   );
 }
 
